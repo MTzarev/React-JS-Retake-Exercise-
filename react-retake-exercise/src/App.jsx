c4a81c9dceb10e31e8620089d42ac5c64589b225
Miroslav Tzarev <mtzarev@gmail.com>
2025-02-26 23:01:19 +0200

    userGreeting conditional rendering

--- react-retake-exercise/src/App.jsx
+++ react-retake-exercise/src/App.jsx
@@ -1,18 +1,21 @@
 import Card from "./Card";
 import Button from "./Button";
-
+import Student from "./Student";
+import UserGreeting from "./UserGreeting";
 function App() {
   
   return (
     <>
 <Card/>
-
 <Button></Button>
+<Student name= "Spongebob" age= {30} isStudent={true}/>
+<UserGreeting isLoggedIn={false} userName = "Miro"/>
 
 </>
 
 );
  
 }
+
 
 export default App
